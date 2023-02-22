02634240dc03eb65eb981a9f1dbf1bb74f1d3bdb
Casey Fleser <cfleser@gmail.com>
2023-02-22 04:24:38 -0600

    Initial Commit

---
+++ TypeSupport/TypeSupportApp.swift
@@ -0,0 +1,17 @@
+//
+//  TypeSupportApp.swift
+//  TypeSupport
+//
+//  Created by Casey Fleser on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct TypeSupportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
